62a79bf924f7bbc0e69ad920762aa8430b338e82
William de Angelo <wdangelo1983@gmail.com>
2022-10-11 13:00:18 -0300

    habilitado cors na aplicação

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe( { errorHttpStatusCode: 422 } ));
   
-  await app.listen(3000);
+  await app.listen(3300);
 }
 bootstrap();
